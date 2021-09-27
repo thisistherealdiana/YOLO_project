3e0171c043eb64f133b731110262e004393d5175
thisisthereldiana <74499545+thisisthereldiana@users.noreply.github.com>
2021-09-27 11:58:02 +0400

    Initial commit (#1)

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _401_kereselidze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
